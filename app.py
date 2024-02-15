6cf88b8bd1b7b4e0395643b0519bdcd2503743ee
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-02-14 22:35:02 -0500

    Finish the app

--- app.py
+++ app.py
@@ -15,17 +15,23 @@ else:
         quit()
 
 random_number = random.randint(0,top_of_range)
-
+guesses = 0
 
 while True:
-	user_guess - input("Make a guess: ")
-	if user_guess.isdigit():
-		user_guess = int(user_guess)
-	else:
-		print('Please type a number larger than 0 next time.')
-		continue
+		guesses += 1
+		user_guess = input("Make a guess: ")
+		if user_guess.isdigit():
+			user_guess = int(user_guess)
+		else:
+			print('Please type a number larger than 0 next time.')
+			continue
 
-	if user_guess == random_number:
-		print("Yout got it!")
-	else:
-		print("You got it wrong!")
+		if user_guess == random_number:
+			print("Yout got it!")
+			break
+		elif user_guess > random_number:
+			print("You were above the number!")
+		else:
+			print("You were below the number!")
+
+print("You got it in",guesses,"guesses")
